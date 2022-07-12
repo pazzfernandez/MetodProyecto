cecd167e54ee3c7dbc9af269ed642f9db9458000
caro890 <carolina.solsona@alumnos.frm.utn.edu.ar>
2022-07-12 01:15:33 -0300

    Algoritmo generador de laberinto terminado

--- ProyectMet/mapas/Casilla.lua
+++ ProyectMet/mapas/Casilla.lua
@@ -5,7 +5,6 @@ Casilla = Object:extend() --Crea la clase Casilla que hereda de la clase Object,
 function Casilla:new(f,c)  --constructor 
   self.f = f  --posición respecto a las filas, usualmente representado con i
   self.c = c  --posición respecto a las columnas, usualmente representado con j
-  self.vecinas = {arriba=true, der=true, abajo=true, izq=true}  
   self.tipo = 0 --valor númerico que representa si será pared(1) o camino(0)
   self.visitada = false --valor boleano que guarda si la casilla ya ha sido visitada durante la generación del laberinto
 end
--- ProyectMet/mapas/generador-de-laberinto.lua
+++ ProyectMet/mapas/generador-de-laberinto.lua
@@ -1,4 +1,11 @@
---Generador de laberinto
+--Generador de laberinto: Backtracking
+--Para generar un laberinto: 
+--    -> en love.load():
+--        llamar a la funcion iniciarLaberinto(altura, base) pasando como argumentos las dimensiones, deben ser numeros impares. Esta devuelve una tabla
+--        llamar luego a la funcion generarAlgoritmo(grilla) pasando como argumento la tabla devuelta por la funcion anterior 
+--    -> en love.draw(): 
+--        llamar a la funcion dibujarLaberinto(grilla, 10) pasando como argumento la tabla devuelta en iniciarLaberinto y el tamaño en el que se dibujaran las casillas
+
 require("nivel/Casilla")  --requiere la clase Casilla contenida en el archivo Casilla.lua
 
 function iniciarLaberinto(altura, base) --esta función inicializa la tabla grilla que será donde se 
@@ -8,6 +15,7 @@ function iniciarLaberinto(altura, base) --esta función inicializa la tabla gril
   --un random salga un nro diferente
   
   grilla = {} --tabla o vector que guarda el laberinto
+  pila = {} --tabla o vector que guardara las casillas ya visitadas que tienen vecinas sin visitar
   
   --llena la grilla con casillas de tipo camino, cada una está rodeada por casillas de tipo pared
   for i=1,altura do
@@ -18,9 +26,11 @@ function iniciarLaberinto(altura, base) --esta función inicializa la tabla gril
       --asigna un tipo(pared o camino) a cada casilla segun su posicion
       if i%2==0 then  
         casilla1.tipo=1
+        casilla1.visitada = true
       else
         if j%2==0 then
           casilla1.tipo=1
+          casilla1.visitada = true
         else
           casilla1.tipo=0
         end
@@ -36,26 +46,30 @@ function iniciarLaberinto(altura, base) --esta función inicializa la tabla gril
   return grilla --devuelve la grilla iniciada
 end
 
-function generarLaberinto(grilla, iActual, jActual) 
+function generarLaberinto(grilla) --funcion que modifica la tabla grilla formando el laberinto, recibe la grilla, y una posicion de la misma desde la cual empezar a generar caminos
   
-  vecinaElegida = {}
-  vecinaElegida.f = 0
-  vecinaElegida.c = 0
+  --establece la casilla actual como la primera
+  iActual = 1 
+  jActual = 1  
+  grilla[iActual][jActual].visitada = true  --marca la casilla actual como visitada
   
-  print("-------------------------")
-  print("Casilla actual: "," i: ", iActual, " j: ",jActual)
-  grilla[iActual][jActual].visitada = true
-  print("Estado: ", grilla[iActual][jActual].visitada)
-  vecinaElegida = mirarVecinas(grilla, grilla[iActual][jActual]) --devuelve una casilla vecina aleatoria que no ha sido visitada
-  
-  if vecinaElegida or vecinaElegida.f~=0 then --vecinaElegida.c~=nil and vecinaElegida.f~=nil then
-    print("Vecina elegida:", vecinaElegida, " i: ", vecinaElegida.f, " j: ", vecinaElegida.c)
-    sacarPared(grilla, vecinaElegida, grilla[iActual][jActual])
-    iActual = vecinaElegida.f
-    jActual = vecinaElegida.c
-    --grilla[iActual][jActual].visitada = true
-    if(iActual > 0 and jActual > 0) then
-      generarLaberinto(grilla, iActual, jActual)
+  while hayCasillas() do  --mientras hayan casillas no visitadas
+    elegida = mirarVecinas(grilla, grilla[iActual][jActual])  --llama a la funcion mirarVecinas y almacena la vecina devuelta en la variable elegida
+    
+    if elegida.f~=0 then  --si la casilla elegida existe
+      insertar(grilla[iActual][jActual])  --la inserta en la pila
+      sacarPared(grilla, elegida, grilla[iActual][jActual]) --saca la pared entre la actual y la elegida
+      --establece como actual a la casilla elegida
+      iActual = elegida.f 
+      jActual = elegida.c
+      grilla[iActual][jActual].visitada = true --la marca como visitada
+      
+    elseif tamanioPila()-1 ~= 0 then  --si la casilla elegida no existe, es decir que la casilla actual se quedo sin vecinas para visitar, y hay casillas en la pila
+      casillaX = extraer()  --extrae la ultima casilla ingresada en la pila
+      --establece como actual la casilla extraida
+      iActual = casillaX.f  
+      jActual = casillaX.c
+      
     end
   end
 end
@@ -76,14 +90,12 @@ function mirarVecinas(grilla, casilla)  --funcion que revisa las casillas que ro
       --que la casilla no ha sido visitada aún
       contador = contador + derecha[1]  --agrega 1 al contador
       table.insert(noVisitadas,contador,derecha[2]) --agrega la casilla al arreglo noVisitadas
-      print("Mirar derecha vecina: ", noVisitadas[contador], " - CONTADOR: ", contador) 
     end 
     
     abajo = mirarAbajo(grilla, casilla) --casilla de abajo
     if abajo[1] ~= 0 then
       contador = contador + abajo[1]
       table.insert(noVisitadas,contador,abajo[2])
-      print("Mirar abajo vecina: ", noVisitadas[contador], " - CONTADOR: ", contador)
     end
     
 
@@ -93,14 +105,12 @@ function mirarVecinas(grilla, casilla)  --funcion que revisa las casillas que ro
     if abajo[1] ~=0 then
       contador = contador + abajo[1]
       table.insert(noVisitadas,contador,abajo[2])
-      print("Mirar abajo vecina: ", noVisitadas[contador], " - CONTADOR: ", contador)
     end
     
     izquierda = mirarIzquierda(grilla, casilla) --casilla izquierda
     if izquierda[1] ~=0 then
       contador = contador + izquierda[1]
       table.insert(noVisitadas,contador,izquierda[2])
-      print("Mirar izquierda vecina: ", noVisitadas[contador], " - CONTADOR: ", contador)
     end
     
   elseif casilla.f==grilla.altura and casilla.c==1 then--si es la esquina inferior izquierda...
@@ -109,14 +119,12 @@ function mirarVecinas(grilla, casilla)  --funcion que revisa las casillas que ro
     if derecha[1] ~=0 then
       contador = contador + derecha[1]
       table.insert(noVisitadas,contador,derecha[2])
-      print("Mirar derecha vecina: ", noVisitadas[contador], " - CONTADOR: ", contador)
     end  
     
     arriba = mirarArriba(grilla, casilla) --casilla de arriba
     if arriba[1] ~=0 then
       contador = contador + arriba[1]
       table.insert(noVisitadas,contador,arriba[2])
-      print("Mirar arriba vecina: ", noVisitadas[contador], " - CONTADOR: ", contador)
     end
     
   elseif casilla.f==grilla.altura and casilla.c==grilla.base then --si es la esquina inferior derecha...
@@ -125,14 +133,12 @@ function mirarVecinas(grilla, casilla)  --funcion que revisa las casillas que ro
     if izquierda[1]~=0 then
       contador = contador + izquierda[1]
       table.insert(noVisitadas,contador,izquierda[2])
-      print("Mirar izquierda vecina: ", noVisitadas[contador], " - CONTADOR: ", contador)
     end 
     
     arriba = mirarArriba(grilla, casilla) --casilla de arriba
     if arriba[1] ~=0 then
       contador = contador + arriba[1]
       table.insert(noVisitadas,contador,arriba[2])
-      print("Mirar arriba vecina: ", noVisitadas[contador], " - CONTADOR: ", contador)
     end
     
   elseif casilla.f==1 then --si es una de la fila superior...
@@ -140,21 +146,18 @@ function mirarVecinas(grilla, casilla)  --funcion que revisa las casillas que ro
     if derecha[1]~=0 then
       contador = contador + derecha[1]
       table.insert(noVisitadas,contador,derecha[2])
-      print("Mirar derecha vecina: ", noVisitadas[contador], " - CONTADOR: ", contador)
     end
     
     abajo = mirarAbajo(grilla, casilla) --casilla de abajo
     if abajo[1]~=0 then
       contador = contador + abajo[1]
       table.insert(noVisitadas,contador,abajo[2])
-      print("Mirar abajo vecina: ", noVisitadas[contador], " - CONTADOR: ", contador)
     end
     
     izquierda = mirarIzquierda(grilla, casilla) --casilla izquierda
     if izquierda[1]~=0 then
       contador = contador + izquierda[1]
       table.insert(noVisitadas,contador,izquierda[2])
-      print("Mirar izquierda vecina: ", noVisitadas[contador], " - CONTADOR: ", contador)
     end
       
   elseif casilla.f==grilla.altura then --si es una de la fila inferior...
@@ -163,21 +166,18 @@ function mirarVecinas(grilla, casilla)  --funcion que revisa las casillas que ro
     if derecha[1]~=0 then
       contador = contador + derecha[1]
       table.insert(noVisitadas,contador,derecha[2])
-      print("Mirar derecha vecina: ", noVisitadas[contador], " - CONTADOR: ", contador)
     end
     
     izquierda = mirarIzquierda(grilla, casilla) --casilla izquierda
     if izquierda[1] ~=0 then
       contador = contador + izquierda[1]
       table.insert(noVisitadas,contador,izquierda[2])
-      print("Mirar izquierda vecina: ", noVisitadas[contador], " - CONTADOR: ", contador)
     end
     
     arriba = mirarArriba(grilla, casilla) --casilla de arriba
     if arriba[1]~=0 then
       contador = contador + arriba[1]
       table.insert(noVisitadas,contador,arriba[2])
-      print("Mirar arriba vecina: ", noVisitadas[contador], " - CONTADOR: ", contador)
     end
     
   elseif casilla.c==1 then --si es una de la columna izquierda...
@@ -186,21 +186,18 @@ function mirarVecinas(grilla, casilla)  --funcion que revisa las casillas que ro
     if derecha[1]~=0 then
       contador = contador + derecha[1]
       table.insert(noVisitadas,contador,derecha[2])
-      print("Mirar derecha vecina: ", noVisitadas[contador], " - CONTADOR: ", contador)
     end
     
     abajo = mirarAbajo(grilla, casilla) --casilla de abajo
     if abajo[1]~=0 then
       contador = contador + abajo[1]
       table.insert(noVisitadas,contador,abajo[2])
-      print("Mirar abajo vecina: ", noVisitadas[contador], " - CONTADOR: ", contador)
     end
     
     arriba = mirarArriba(grilla, casilla) --casilla de arriba
     if arriba[1]~=0 then
       contador = contador + arriba[1]
       table.insert(noVisitadas,contador,arriba[2])
-      print("Mirar arriba vecina: ", noVisitadas[contador], " - CONTADOR: ", contador)
     end
     
   elseif casilla.c==grilla.base then --si es una de la columna derecha...
@@ -209,21 +206,18 @@ function mirarVecinas(grilla, casilla)  --funcion que revisa las casillas que ro
     if abajo[1]~=0 then 
       contador = contador + abajo[1]
       table.insert(noVisitadas,contador,abajo[2])
-      print("Mirar abajo vecina: ", noVisitadas[contador], " - CONTADOR: ", contador)
     end
     
     izquierda = mirarIzquierda(grilla, casilla) --casilla izquierda
     if izquierda[1]~=0 then
       contador = contador + izquierda[1]
       table.insert(noVisitadas,contador,izquierda[2])
-      print("Mirar izquierda vecina: ", noVisitadas[contador], " - CONTADOR: ", contador)
     end
     
     arriba = mirarArriba(grilla, casilla) --casilla de arriba
     if arriba[1]~=0 then
       contador = contador + arriba[1]
       table.insert(noVisitadas,contador,arriba[2])
-      print("Mirar arriba vecina: ", noVisitadas[contador], " - CONTADOR: ", contador)
     end
     
   else --si es una del centro...
@@ -232,40 +226,30 @@ function mirarVecinas(grilla, casilla)  --funcion que revisa las casillas que ro
     if derecha[1]~=0 then
       contador = contador + derecha[1]
       table.insert(noVisitadas,contador,derecha[2])
-      print("Mirar derecha vecina: ", noVisitadas[contador], " - CONTADOR: ", contador)
     end
     
     abajo = mirarAbajo(grilla, casilla) --casilla de abajo
     if abajo[1]~=0 then
       contador = contador + abajo[1]
       table.insert(noVisitadas,contador,abajo[2])
-      print("Mirar abajo vecina: ", noVisitadas[contador], " - CONTADOR: ", contador)
     end
     
     izquierda = mirarIzquierda(grilla, casilla) --casilla izquierda
     if izquierda[1]~=0 then
       contador = contador + izquierda[1]
       table.insert(noVisitadas,contador,izquierda[2])
-      print("Mirar izquierda vecina: ", noVisitadas[contador], " - CONTADOR: ", contador)
     end
     
     arriba = mirarArriba(grilla, casilla) --casilla de arriba
     if arriba[1]~=0 then
       contador = contador + arriba[1]
       table.insert(noVisitadas,contador,arriba[2])
-      print("Mirar arriba vecina: ", noVisitadas[contador], " - CONTADOR: ", contador)
     end
     
   end
   
-  print("CONTADOR: ", contador)
-  if contador~=0 then --si hay casillas no visitadas
-    for i=1,contador,1 do
-      print(i," - ",noVisitadas[i])
-    end
-    
+  if contador~=0 then --si hay casillas no visitadas    
     numAleatorio = math.random(1,contador)  --elige una al azar entre las disponibles
-    
     vecina = noVisitadas[numAleatorio] --le asigna a la casilla Vecina, la casilla elegida
   end
   
@@ -318,25 +302,52 @@ function sacarPared(grilla, proxima, actual)  --funcion que convierte la pared e
     if(proxima.c > actual.c) then
       grilla[actual.f][proxima.c-1].tipo = 0
       grilla[actual.f][proxima.c-1].visitada = true
-      print("PARED REMOVIDA: ", grilla[actual.f][proxima.c-1].f, " - ", grilla[actual.f][proxima.c-1].c)
     elseif(proxima.c < actual.c) then 
       grilla[actual.f][proxima.c+1].tipo = 0
       grilla[actual.f][proxima.c+1].visitada = true
-      print("PARED REMOVIDA: ", grilla[actual.f][proxima.c+1].f, " - ", grilla[actual.f][proxima.c+1].c)
     end
   elseif(proxima.c == actual.c) then  --si estan en la misma columna
     if(proxima.f > actual.f) then
       grilla[proxima.f-1][actual.c].tipo = 0
       grilla[proxima.f-1][actual.c].visitada = true
-      print("PARED REMOVIDA: ", grilla[proxima.f-1][actual.c].f," - ", grilla[proxima.f-1][actual.c].c)
     elseif(proxima.f < actual.f) then
       grilla[proxima.f+1][actual.c].tipo = 0
       grilla[proxima.f+1][actual.c].visitada = true
-      print("PARED REMOVIDA: ", grilla[proxima.f+1][actual.c].f," - ", grilla[proxima.f+1][actual.c].c)
     end
   end
 end
 
+function insertar(cas)  --funcion para insertar una casilla de la pila
+  p = tamanioPila()
+  table.insert(pila,p,cas)
+end
+
+function extraer() --funcion para extraer una casilla de la pila
+  ult = tamanioPila()-1
+  c = pila[ult]
+  table.remove(pila,ult)
+  return c
+end
+
+function tamanioPila() --funcion que devuelve la cantidad de casillas disponibles en la pila
+  pos = 1
+  for i, valor in ipairs(pila) do
+    pos = pos + 1
+  end
+  --print(pos)
+  return pos
+end
+
+function hayCasillas() --funcion que revisa si hay casillas que aun no han sido visitadas
+  for i=1,grilla.altura,1 do
+    for j=1,grilla.base,1 do
+      if grilla[i][j].visitada==false then
+        return true
+      end
+    end
+  end
+  return false
+end
 
 function dibujarLaberinto(grilla,dimCasilla) 
   for i=1,grilla.altura do  
@@ -346,10 +357,10 @@ function dibujarLaberinto(grilla,dimCasilla)
       else
         love.graphics.setColor(255, 0, 0, 1) 
       end
-      if grilla[i][j].visitada then
+      if grilla[i][j].visitada and grilla[i][j].tipo == 0  then
         love.graphics.setColor(1, 0, 1)
       end
       love.graphics.rectangle("fill", j * dimCasilla, i * dimCasilla, dimCasilla, dimCasilla)
     end
-end  
+  end  
 end
