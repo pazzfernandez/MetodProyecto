11bc8a5360930cbf48a717bbc8a08f61303b12fb
caro890 <carolina.solsona@alumnos.frm.utn.edu.ar>
2022-08-02 17:56:23 -0300

    Update main.lua

--- ProyectMet/main.lua
+++ ProyectMet/main.lua
@@ -555,8 +555,6 @@ function crearBala()
     table.insert(balas, bala)
 end
 
-
-
 --Funcion para calcular la distancia entre dos coordenadas en la pantalla
 function distanciaEntre(x1, y1, x2, y2)
     return math.sqrt( (x2 - x1)^2 + (y2 - y1)^2 )
